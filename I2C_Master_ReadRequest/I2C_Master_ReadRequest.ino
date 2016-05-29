ce004a2cb497660313561cb445c5b2d9d155841a
hedgesjack <hedgesjack@hotmail.co.uk>
2016-05-29 16:25:37 +0100

    Basic Code
    Basic code used to communicate between a 2560 as master and Uno as slave
    to send a ADC data packet between devices

---
+++ I2C_Master_ReadRequest/I2C_Master_ReadRequest.ino
@@ -0,0 +1,42 @@
+//                                    //
+//  Title:   I2C_Master_ReadRequest   //
+//  Author:  J Hedges                 //
+//  Date:    29/05/2016               //
+//  Board:   2560                     //
+//                                    //
+
+//  Library Declarations  //
+#include <Wire.h>
+
+//  Global Variable Declarations  //
+//      none used
+
+//  Startup Code - executes once  //
+void setup()
+{
+  Wire.begin();   //  Join I2C bus as master (no address)
+  Serial.begin(9600);   //  Start serial communiation
+}
+
+//  Main Program Loop   //
+void loop() 
+{
+  //  Requet Data from slave writer
+  Wire.requestFrom(3, 2);    // Address 3, Two BYTES
+  //  Read everything that is at the port
+  while (Wire.available()) 
+  {
+    byte a = Wire.read();   //  Receive first byte
+    byte b = Wire.read();   //  Recieve second byte
+    int c = a;    //  Set first byte as integer in variable c
+    c = (a << 8) | b;   //  shift c by 8 BITS to correct transmission formatting and OR it with the second BYTE B
+    Serial.print("Data Recieved ");    //  Print the result to the serial com port for debug
+    Serial.print(c);    //  Print the result to the serial com port for debug
+    Serial.print("\n");   //  Add carriage return to each line to allow for easy reading in com port
+  }
+  delay(500);   // Stop software running too quick and stressing CPU, can be used as sofware timing
+}
+
+//                //
+//  END OF FILE   //
+//                //
---
+++ I2C_Slave_WriteOnRequest/I2C_Slave_WriteOnRequest.ino
@@ -0,0 +1,44 @@
+//                                    //
+//  Title:   I2C_Master_ReadRequest   //
+//  Author:  J Hedges                 //
+//  Date:    29/05/2016               //
+//  Board:   Uno                      //
+//                                    //
+
+//  Library Declarations  //
+#include <Wire.h>
+
+//  Global Variable Declarations  //
+int Data = 0;
+byte myArray[2];
+
+//  Startup Code - executes once  //
+void setup() 
+{
+  Wire.begin(3);    //  Join I2C bus as slave with address 3
+  Wire.onRequest(requestEvent);   //  Register event for data request
+  Serial.begin(9600);   //  Start serial communication
+}
+
+//   Main Program Loop  //
+void loop() 
+{
+  Data = analogRead(A0);  //  Read Analogue pin A0
+  Serial.print("Data Sent ");    //  Print the result to the serial com port for debug
+  Serial.print(Data);   //  Print the result to the serial com port for debug
+  Serial.print("\n");   //  Add carriage return to com port so data is readable
+  myArray[0] = (Data >> 8) & 0xFF;    //  Shift number down by 8BITS and mask with 0xFF to remove other bits, place in array 0
+  myArray[1] = Data & 0xFF;   //  Format lower part by masking with 0xFF 8BIT to remove any other bits, place in array 1
+  delay(500);   // Stop software running too quick and stressing CPU, can be used as sofware timing
+}
+
+//  
+// this function is registered as an event, see setup()
+void requestEvent() 
+{
+  Wire.write(myArray, 2);   //  Send array of data that is 2 BYTES in size
+}
+
+//                //
+//  END OF FILE   //
+//                //
